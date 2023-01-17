f7cef374122ffe2be5b2db9d101183858583960e
Ozanne Alexander <alexanoz@outlook.com>
2023-01-16 21:52:48 -0500

    created the require packages and added two files to staged for commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var inquirer = require ('inquirer');
+
+// Put your required packages here:
+const inquirer = require ('inquirer');
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(fileDirectory, "index.html");
+
+
+// Modules
+
+
+
+
 inquirer
     .prompt([
 
