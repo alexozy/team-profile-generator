2a6ee8220b5234669eab5ec170e1d5d46907b516
Ozanne Alexander <alexanoz@outlook.com>
2023-01-17 06:34:03 -0500

    successfully got the questions to run through the command line!; also wrote the skeleton code for writeHTML function

--- index.js
+++ index.js
@@ -17,38 +17,9 @@ const genHTML = require("./lib/generateHTML")
 // We will need an empty array for the input-ed employees
 const teamArray = [];
 
-// now let's make our question array:
-
-const questions = [
- // example: type: "list"
-        // name: ""
-        // message: ""
-        // choices: [array of choices] *if needed*
-
-       {
-        type: "input",
-        name: "name",
-        message: "Please provide the employee name:"
-       },
-       {
-        type: "input",
-        name: "email",
-        message: "Please provide the employee email:"
-       },
-       {
-           type: "input",
-           name: "id",
-           message: "Please provide the employee's unique ID:"
-        },
-        {
-         type: "list",
-         name: "role",
-         message: "Please define the employee's role:",
-         choices: ["Intern", "Manager", "Engineer"],
-        }, 
-    ]
-
-    // we have 3 roles of employees please make questions for each role:
+// now let's make our function and .prompt the questions:
+ // we have 3 roles of employees please make questions for each role:
+ // I want to try the function init () approach similar to last challenge, i believe it will work here
  // example: type: "list"
         // name: ""
         // message: ""
@@ -56,9 +27,40 @@ const questions = [
 
 //  Intern Goes here : inQuest
 
-        inQuest = []
-// Manager goes here: manQuest
-        manQuest = []
-// Engineer goes here: engQuest
-        engQuest = []
+//         inQuest = []
+// // Manager goes here: manQuest
+//         manQuest = []
+// // Engineer goes here: engQuest
+//         engQuest = []
 
+    function init () {
+        inquirer
+        .prompt ([
+            {
+                type: "input",
+                name: "name",
+                message: "Please provide the employee name:"
+               },
+               {
+                type: "input",
+                name: "email",
+                message: "Please provide the employee email:"
+               },
+               {
+                   type: "input",
+                   name: "id",
+                   message: "Please provide the employee's unique ID:"
+                },
+                {
+                 type: "list",
+                 name: "role",
+                 message: "Please define the employee's role:",
+                 choices: ["Intern", "Manager", "Engineer"],
+                }, 
+
+        ])
+// your write to HTML will go here
+        .then(() => {});
+
+    }
+    init ();
