a53fc3ce411883b00b392702d26c5d581e892491
Ozanne Alexander <alexanoz@outlook.com>
2023-01-16 22:35:13 -0500

    created questions array for function;

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+function init() {
 // Put your required packages here:
 const inquirer = require ('inquirer');
 const fs = require("fs");
@@ -6,7 +7,7 @@ const path = require("path");
 const filePath = path.join(fileDirectory, "index.html");
 
 
-// Modules
+// Modules & path
 const Employee = require("./lib/Employee")
 const Intern = require("./lib/Intern")
 const Engineer = require("./lib/Engineer")
@@ -16,6 +17,42 @@ const genHTML = require("./lib/generateHTML")
 
 
 inquirer
+// questions prompt
     .prompt([
+ // type: "list"
+        // name: ""
+        // message: ""
+        // choices: [array of choices] *if needed*
+
+       {
+        type: "input",
+        name: "name",
+        message: ""
+       },
+       {
+        type: "input",
+        name: "email",
+        message: ""
+       },
+       {
+        type: "input",
+        name: "role",
+        message: ""
+       }, 
+       {
+        type: "input",
+        name: "",
+        message: ""
+       },
+       {
+        type: "list",
+        name: "",
+        message: ""
+       },
+
+
+
+
 
     ])
+}
