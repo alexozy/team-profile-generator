93afcfd5323acb4170613c404913193c4bb5d69b
Ozanne Alexander <alexanoz@outlook.com>
2023-01-17 23:07:29 -0500

    started working on the readme file for project

---
+++ README.md
@@ -0,0 +1,40 @@
+[![License: MIT] (https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/license/MIT)
+
+## **Description**
+## Table of Contents
+    [Description](#description)
+    [Installation](#installation)
+    [Usage](#usage)
+    [License](#license)
+    [Contributors](#contributors)
+    [Test](#tests)
+    [Questions](#questions)
+## Installation
+     In order to install this application, the user would first need to INSTALL the code to  your terminal inside your unique repository. Utilize "npm init" on your command line terminal. To install your Inquirer enter ```npm install inquirer``` . To run the program enter ```node index.js``` .
+     
+*Please see the example code below:*
+    `/`/`/`JavaScript
+    
+    npm init
+
+    npm install inquirer
+
+    const inquirer = require('inquirer');
+
+    `/`/`
+
+## Usage
+
+## License
+
+## Contributors
+
+## Test
+
+## Questions
+    If you have questions please reach me at:
+    **GitHub**
+    https://alexozy.github.io/team-profile-generator
+
+    **Email**
+    alexanoz@outlook.com
--- index.js
+++ index.js
@@ -17,6 +17,29 @@ const genHTML = require("./lib/generateHTML")
 // We will need an empty array for the input-ed employees
 const teamArray = [];
 
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please provide the employee name:"
+       },
+       {
+        type: "input",
+        name: "email",
+        message: "Please provide the employee email:"
+       },
+       {
+           type: "input",
+           name: "id",
+           message: "Please provide the employee's unique ID:"
+        },
+        {
+         type: "list",
+         name: "role",
+         message: "Please define the employee's role:",
+         choices: ["Intern", "Manager", "Engineer"],
+        }, 
+]
 // now let's make our function and .prompt the questions:
  // we have 3 roles of employees please make questions for each role:
  // I want to try the function init () approach similar to last challenge, i believe it will work here
@@ -33,34 +56,37 @@ const teamArray = [];
 // // Engineer goes here: engQuest
 //         engQuest = []
 
-    function init () {
-        inquirer
-        .prompt ([
-            {
-                type: "input",
-                name: "name",
-                message: "Please provide the employee name:"
-               },
-               {
-                type: "input",
-                name: "email",
-                message: "Please provide the employee email:"
-               },
-               {
-                   type: "input",
-                   name: "id",
-                   message: "Please provide the employee's unique ID:"
-                },
-                {
-                 type: "list",
-                 name: "role",
-                 message: "Please define the employee's role:",
-                 choices: ["Intern", "Manager", "Engineer"],
-                }, 
 
-        ])
-// your write to HTML will go here
-        .then(() => {});
 
-    }
-    init ();
+
+//     function init () {
+//         inquirer
+//         .prompt ([
+//             {
+//                 type: "input",
+//                 name: "name",
+//                 message: "Please provide the employee name:"
+//                },
+//                {
+//                 type: "input",
+//                 name: "email",
+//                 message: "Please provide the employee email:"
+//                },
+//                {
+//                    type: "input",
+//                    name: "id",
+//                    message: "Please provide the employee's unique ID:"
+//                 },
+//                 {
+//                  type: "list",
+//                  name: "role",
+//                  message: "Please define the employee's role:",
+//                  choices: ["Intern", "Manager", "Engineer"],
+//                 }, 
+
+//         ])
+// // your write to HTML will go here
+//         .then(() => {});
+
+//     }
+//     init ();
