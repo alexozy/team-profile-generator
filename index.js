1b988654d5f9b71a1fdfe174ec1ebf49b22e75f0
Ozanne Alexander <alexanoz@outlook.com>
2023-01-19 08:22:18 -0500

    edited notes and cleaned up old code replaced by inquirer functions

--- index.js
+++ index.js
@@ -16,78 +16,16 @@ const genHTML = require("./lib/generateHTML")
 // We will need an empty array for the input-ed employees
 const squad = [];
 
-
-// we have 3 roles of employees please make questions for each role:
-// I want to try the function init () approach similar to last challenge, i believe it will work here
-const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Please provide the employee name:"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please provide the employee email:"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Please provide the employee's unique ID:"
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "Please define the employee's role:",
-        choices: ["Intern", "Manager", "Engineer"],
-    },
-]
-
-// Create the required prompts for the intern here | requires school input
-intQuest = [
-    {
-        type: "input",
-        name: "school",
-        message: "Please input the intern's school:",
-        // we need something to make this field required:
-        validate: school => {
-            // insert your if statement here for no input message
-            if (school){
-                return true;
-            } else {
-                console.log ("School name is required!")
-                return false;
-            }
-        }
-    }
-]
-
-
-// Create the required prompts for the Manager here| requires their officeNumber
-
-mgmtQuest = [
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "Insert this manager's office number:",
-        // we need something to make this field required:
-        validate: officeNumber => {
-            // insert your if statement here for no input message
-            if (officeNumber){
-                return true;
-            } else {
-                console.log ("An office number is required!")
-                return false;
-            }
-        }
-    }
-]
-
-
-
 // Place the function to cycle through prompts here
 const addNew = () =>{
     console.log ('Build Your Squad')
+     // TODO Create the required prompts for the Engineer here | their github is required! example below
+            // validate: officeNumber => {
+        //     // insert your if statement here for no input message
+        //     if (officeNumber){
+        //         return true;
+        //     } else {console.log ("An office number is required!")
+        //         return false;}}
     return inquirer.prompt([
         {
             type: "list",
@@ -95,31 +33,6 @@ const addNew = () =>{
             message: "Please define the employee's role:",
             choices: ["Intern", "Manager", "Engineer"],
         },
-        {
-            type: "input",
-            name: "name",
-            message: "Please provide the employee name:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please provide the employee email:",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please provide the employee's unique ID:",
-        },
-        // TODO Create the required prompts for the Engineer here | their github is required!
-            // validate: officeNumber => {
-        //     // insert your if statement here for no input message
-        //     if (officeNumber){
-        //         return true;
-        //     } else {
-        //         console.log ("An office number is required!")
-        //         return false;
-        //     }
-        // }
         {
             type: "input",
             name: "officeNumber",
@@ -163,6 +76,21 @@ const addNew = () =>{
             },
         },
         {
+            type: "input",
+            name: "name",
+            message: "Please provide the employee name:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please provide the employee email:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please provide the employee's unique ID:",
+        },
+        {
             type: "confirm",
             name: "addOne",
             message: "Do you have another squad member?",
@@ -172,7 +100,10 @@ const addNew = () =>{
     .then (userInput =>{
         let{role, name, ID, email, school, github, addOne} = userInput;
         let employee;
-// give the respective inputs a home
+// TODO give the respective inputs a home | if statements
+        if(role === "Manager"){} 
+        if(role === "Engineer"){}
+        if(role === "Intern"){}
 
     })
 }
