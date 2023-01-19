66cedf89c535dd91a825766772b0f3224b63a40b
Ozanne Alexander <alexanoz@outlook.com>
2023-01-19 07:50:52 -0500

    placed all employee prompts into one variable; will attempt to get the function to require inputs using validate

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-// const filePath = path.join(fileD, 'index.html');
 
 // Modules & path
 const Employee = require("./lib/Employee")
@@ -15,34 +14,34 @@ const Manager = require("./lib/Manager")
 const genHTML = require("./lib/generateHTML")
 
 // We will need an empty array for the input-ed employees
-const teamArray = [];
+const squad = [];
 
 
 // we have 3 roles of employees please make questions for each role:
 // I want to try the function init () approach similar to last challenge, i believe it will work here
-// const questions = [
-//     {
-//         type: "input",
-//         name: "name",
-//         message: "Please provide the employee name:"
-//     },
-//     {
-//         type: "input",
-//         name: "email",
-//         message: "Please provide the employee email:"
-//     },
-//     {
-//         type: "input",
-//         name: "id",
-//         message: "Please provide the employee's unique ID:"
-//     },
-//     {
-//         type: "list",
-//         name: "role",
-//         message: "Please define the employee's role:",
-//         choices: ["Intern", "Manager", "Engineer"],
-//     },
-// ]
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please provide the employee name:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please provide the employee email:"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please provide the employee's unique ID:"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "Please define the employee's role:",
+        choices: ["Intern", "Manager", "Engineer"],
+    },
+]
 
 // Create the required prompts for the intern here | requires school input
 intQuest = [
@@ -84,56 +83,73 @@ mgmtQuest = [
     }
 ]
 
-// Create the required prompts for the Engineer here | their github is required!
-engQuest = [
-    {
-        type: "input",
-        name: "github",
-        message: "Please disclose the Engineer's github username:",
-        // we need something to make this field required:
-        validate: github => {
-            // insert your if statement here for no input message
-            if (github){
-                return true;
-            } else {
-                console.log ("A github username is required!")
-                return false;
-            }
-        }
-    }
-]
+
 
 // Place the function to cycle through prompts here
-const managerAdd = () =>{
+const addNew = () =>{
+    console.log ('Build Your Squad')
     return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Please provide the employee name:"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please provide the employee email:"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Please provide the employee's unique ID:"
-        },
         {
             type: "list",
             name: "role",
             message: "Please define the employee's role:",
             choices: ["Intern", "Manager", "Engineer"],
         },
+        {
+            type: "input",
+            name: "name",
+            message: "Please provide the employee name:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please provide the employee email:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please provide the employee's unique ID:",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            when: (input) => input.role === "Manager",
+            message: "Please insert the Manager's officeNumber:",
+        },
+        // Create the required prompts for the Engineer here | their github is required!
+        {
+            type: "input",
+            name: "github",
+            when: (input)=> input.role === "Engineer",
+            message: "Please provide the Engineer's github username:",
+        },
+        {
+            type: "input",
+            name: "school",
+            when: (input)=> input.role === "Intern",
+            message: "Please provide the name of the Intern's school:",
+        },
+        {
+            type: "confirm",
+            name: "addOne",
+            message: "Do you need to add another member of your squad?"
+        },
     ])
+    .then (userInput =>{
+        let{role, name, ID, email, school, github, addOne}
+    })
 }
 
-// you should have a function that walks the user through adding a new team member
-const employeeAdd = () => {
+// validate: officeNumber => {
+//     // insert your if statement here for no input message
+//     if (officeNumber){
+//         return true;
+//     } else {
+//         console.log ("An office number is required!")
+//         return false;
+//     }
+// }
 
-}
 
 // Don't forget to call your function!
-managerAdd();
+memberAdd();
